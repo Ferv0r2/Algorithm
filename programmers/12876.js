e208e96a5c4dc64fcff6501f4a68eb886fe822a1
황원태 <amlk31255@gmail.com>
2023-03-27 14:40:49 +0900

    프로그래머스 겹치는 선분의 길이

---
+++ programmers/12876.js
@@ -0,0 +1,35 @@
+// 겹치는 선분의 길이
+
+function solution(lines) {
+  let result = 0;
+  let lineMap = new Array(200).fill(0);
+
+  for (let i = 0; i < 3; i++) {
+    let left = lines[i][0];
+    let right = lines[i][1];
+
+    for (let j = left; j < right; j++) {
+      lineMap[j + 100] += 1;
+    }
+  }
+
+  for (let i in lineMap) {
+    if (lineMap[i] > 1) {
+      result += 1;
+    }
+  }
+
+  return result;
+}
+
+// 모범 답안
+
+function solution(lines) {
+  let line = new Array(200).fill(0);
+
+  lines.forEach(([a, b]) => {
+    for (; a < b; a++) line[a + 100]++;
+  });
+
+  return line.reduce((a, c) => (c > 1 ? a + 1 : a), 0);
+}
